2162e3e8e0b8b0e31c6e6715658bb67f0179c308
markej1 <xmjarx@gmail.com>
2023-10-21 18:37:09 +0200

    fixed init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import { StartComponent } from './start/start.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        StartComponent
+        AppComponent
     ],
     imports: [
         BrowserModule
