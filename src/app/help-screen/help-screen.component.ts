7426b1b9752a3a54ebb45a095a7c954ef9f73d89
markej1 <xmjarx@gmail.com>
2023-10-24 16:59:35 +0200

    html files added

--- src/app/help-screen/help-screen.component.ts
+++ src/app/help-screen/help-screen.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
 
@@ -8,69 +8,7 @@ import {MatIconModule} from "@angular/material/icon";
     selector: 'app-help-screen',
     standalone: true,
     imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
-    template: `
-    <div class="header">
-        <h3 mat-dialog-title>Zajęcia obowiązkowe</h3>
-        <button mat-icon-button mat-dialog-close>
-            <mat-icon>close</mat-icon>
-        </button>
-    </div>
-    <div class="grid">
-      <div class="column">
-        <div class="circle" style="background-color: #ffed80"></div>
-        <p class="text">kierunkowe</p>
-        <div class="circle" style="background-color: #fd9999"></div>
-        <p class="text">specjalnościowe</p>
-        <div class="circle" style="background-color: #aa00ff"></div>
-        <p class="text">praca dyplomowa</p>
-      </div>
-      <div class="column">
-        <div class="circle" style="background-color: #95daf3"></div>
-        <p class="text">z zakresu nauk podstawowych</p>
-        <div class="circle" style="background-color: #abfa98"></div>
-        <p class="text">kształcenia ogólnego</p>
-        <div class="circle" style="background-color: #6c8dff"></div>
-        <p class="text">praktyka</p>
-      </div>
-    </div>
-    <h3 mat-dialog-title>Bloki wybieralne</h3>
-    <div class="grid">
-      <div class="column">
-        <div class="circle" style="background-color: #d2b60d"></div>
-        <p class="text">kierunkowe</p>
-        <div class="circle" style="background-color: #b92828"></div>
-        <p class="text">specjalnościowe</p>
-      </div>
-      <div class="column">
-        <div class="circle" style="background-color: #1a9ac0"></div>
-        <p class="text">z zakresu nauk podstawowych</p>
-        <div class="circle" style="background-color: #68b934"></div>
-        <p class="text">kształcenia ogólnego</p>
-      </div>
-    </div>
-    <div class="bottom_grid">
-      <div>
-
-      </div>
-      <div class="grid2">
-        <div>
-          liczba punktów ECTS z podziałem na formy kształcenia <br>
-          nazwa przedmiotu
-        </div>
-        <div class="rectangle">
-
-        </div>
-        <div>
-          przedmiot kończy się egzaminem <br>
-          2h - wykład <br>
-          2h - ćwiczenia <br>
-          0h - laboratorium <br>
-          0h - projekt <br>
-          0h - seminarium <br>
-        </div>
-      </div>
-    </div>
-  `,
+    templateUrl: './help-screen.component.html',
     styleUrls: ['./help-screen.component.css']
 })
 export class HelpScreenComponent {
--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {HelpScreenComponent} from "../help-screen/help-screen.component";
@@ -8,39 +8,16 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 
 @Component({
-  selector: 'app-plan',
-  standalone: true,
+    selector: 'app-plan',
+    standalone: true,
     imports: [CommonModule, MatButtonModule, MatDialogModule, MatIconModule, MatInputModule, MatSelectModule],
-  template: `
-      <div class="header">
-          <p>
-              Informatyka stosowana <br>
-              Start 2021/2022
-          </p>
-          <mat-form-field>
-              <mat-label>Select an option</mat-label>
-              <mat-select>
-                  <mat-option value="1">Option 1</mat-option>
-                  <mat-option value="2">Option 2</mat-option>
-                  <mat-option value="3">Option 3</mat-option>
-              </mat-select>
-          </mat-form-field>
-      </div>
-      <div class="footer">
-          <p>Politechnika wrocławska</p>
-
-          <button mat-icon-button (click)="openDialog()">
-              <mat-icon>help</mat-icon>
-          </button>
-
-
-      </div>
-  `,
-  styleUrls: ['./plan.component.css']
+    templateUrl: './plan.component.html',
+    styleUrls: ['./plan.component.css']
 })
 
 export class PlanComponent {
-    constructor(public dialog: MatDialog) {}
+    constructor(public dialog: MatDialog) {
+    }
 
     openDialog() {
         const dialogRef = this.dialog.open(HelpScreenComponent);
